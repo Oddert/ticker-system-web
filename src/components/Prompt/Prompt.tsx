ed301cffc1069005961dbbe9c52d6b10690973f4
Robyn F H Veitch <oddert@gmail.com>
2024-08-13 19:31:02 +0100

    added prompt viewer modal to prompt

--- src/components/Prompt/Prompt.tsx
+++ src/components/Prompt/Prompt.tsx
@@ -1,25 +1,42 @@
-import { FC } from 'react';
+import { FC, Fragment, useState } from 'react';
 
 import { Button, Typography } from '@mui/material';
 
 import type { iPrompt } from '../../types/prompt';
+
+import PromptViewer from '../PromptViewer';
 
 interface iProps {
     prompt: iPrompt
 }
 
 const Prompt: FC<iProps> = ({ prompt }) => {
+    const [open, setOpen] = useState(false)
+
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
     return (
-        <Button sx={(theme) => ({
-            alignItems: 'flex-start',
-            color: theme.palette.text.primary,
-            background: theme.palette.secondary.main,
-            padding: '16px 24px',
-            justifyContent: 'flex-start',
-            width: '40vw',
-        })}>
-            <Typography variant='h4'>{prompt.title}</Typography>
-        </Button>
+        <Fragment>
+            <Button
+                onClick={handleOpen}
+                sx={(theme) => ({
+                    alignItems: 'flex-start',
+                    color: theme.palette.text.primary,
+                    background: theme.palette.secondary.main,
+                    padding: '16px 24px',
+                    justifyContent: 'flex-start',
+                    width: '40vw',
+                })}
+            >
+                <Typography variant='h4'>{prompt.title}</Typography>
+            </Button>
+            <PromptViewer
+                open={open}
+                onClose={handleClose}
+                prompt={prompt}
+            />
+        </Fragment>
     )
 }
 
