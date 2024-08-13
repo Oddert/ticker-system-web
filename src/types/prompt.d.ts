a9c3d88c851f43e13bc1f45184827875cc4e1b11
Robyn F H Veitch <oddert@gmail.com>
2024-08-13 19:29:46 +0100

    updated prompt to include additional attributes

--- src/types/prompt.d.ts
+++ src/types/prompt.d.ts
@@ -3,4 +3,21 @@ export interface iPrompt {
     title: string;
     defaultDeferQuantity: number;
     defaultDeferPeriod: 'day' | 'month' | 'year';
+    id: string;// uuid
+    description: string | null; // str(2000)
+    checklist: string | null; // FK.checklist
+    links: string[]; // [] FK.links
+    createdOn: string; // date
+    updatedOn: string | null; // date
+    deletedOn: string | null; // date
+    deleted: boolean; // boolean
+    status: 'open' | 'resolved' | 'archived' | 'deleted';
+    deferredCount: number; // number (default: 0)
+    criticality: 'severe' | 'default' | 'reminder' | 'reference';
 }
+
+// checklist (one to one prompt)
+
+// checklist item (many to one checklist)
+
+// links (many to one)
